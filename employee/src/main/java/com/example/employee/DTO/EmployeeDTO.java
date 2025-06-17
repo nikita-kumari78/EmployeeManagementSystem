447b1b40f791df1480a73c55ebb3abd3c6d0bb57
nikita-kumari78 <109480916+nikita-kumari78@users.noreply.github.com>
2025-06-17 22:34:49 +0530

    EmployeeDTO.java

--- employee/src/main/java/com/example/employee/DTO/EmployeeDTO.java
+++ employee/src/main/java/com/example/employee/DTO/EmployeeDTO.java
@@ -1,25 +1,19 @@
 package com.example.employee.DTO;
 import lombok.Data;
-
 import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDate;
 
-	import java.time.LocalDate;
-
-	@Data
-	public class EmployeeDTO {
-		private Long id;
-	    private String name;
-	    private String department;
-	    private double salary;
-	   
-
-	    
-
-	    public EmployeeDTO(Long id, String name, String department, Double salary, LocalDate dateOfJoining) {
-	        this.id = id;
-	        this.name = name;
-	        this.department = department;
-	        this.salary = salary;
+@Data
+public class EmployeeDTO {
+	private Long id;
+	private String name;
+	private String department;
+	 private double salary;
+public EmployeeDTO(Long id, String name, String department, Double salary, LocalDate dateOfJoining) {
+         this.id = id;
+	 this.name = name;
+	 this.department = department;
+	 this.salary = salary;
 	       
 	    }
 	}
